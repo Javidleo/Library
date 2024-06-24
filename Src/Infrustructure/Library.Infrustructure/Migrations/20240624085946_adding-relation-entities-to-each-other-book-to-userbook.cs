3ccd34d4de3a90daabd7247988ca7e44101c7e90
mehdi_807 <mehdij807@gmail.com>
2024-06-24 17:21:32 +0330

    adding Migration

---
+++ Src/Infrustructure/Library.Infrustructure/Migrations/20240624085946_adding-relation-entities-to-each-other-book-to-userbook.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Infrustructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingrelationentitiestoeachotherbooktouserbook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
