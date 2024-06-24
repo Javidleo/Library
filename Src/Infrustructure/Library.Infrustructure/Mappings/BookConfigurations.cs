1270d6b2dee6fb8dc8af644487f4c00aa7ea3439
mehdi_807 <mehdij807@gmail.com>
2024-06-24 17:12:06 +0330

    changing some relations in Book mapping

--- Src/Infrustructure/Library.Infrustructure/Mappings/BookConfigurations.cs
+++ Src/Infrustructure/Library.Infrustructure/Mappings/BookConfigurations.cs
@@ -12,10 +12,22 @@ public class BookConfigurations : IEntityTypeConfiguration<Book>
 {
     public void Configure(EntityTypeBuilder<Book> builder)
     {
+        builder.HasMany(i => i.UserBooks)
+               .WithOne(i => i.Book)
+               .HasForeignKey(i => i.BookId)
+               .OnDelete(DeleteBehavior.ClientSetNull);
+
         builder.HasKey(i => i.Id);
         
         builder.Property(i => i.Title)
                .HasMaxLength(20)
+               .IsRequired();
+
+        builder.Property(i => i.Publisher)
+               .HasMaxLength(20)
+               .IsRequired();
+
+        builder.Property(i => i.Price)
                .IsRequired();
     }
 }
