c5a5773bad28788bd044fd2868e4d224279a421b
mehdi_807 <mehdij807@gmail.com>
2024-06-24 17:18:12 +0330

    deleted wrong props that related to wrong entity

---
+++ Src/API/Library.API/Dtos/BookDto.cs
@@ -0,0 +1,22 @@
+﻿namespace Library.API.Dtos;
+
+public class BookDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Publisher { get; set; }
+    public decimal Price { get; set; }
+}
+public class UpdateBookDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Publisher { get; set; }
+    public decimal Price { get; set; }
+}
+public class RenameBook
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+
+}
