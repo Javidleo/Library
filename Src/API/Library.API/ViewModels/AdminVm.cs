15102e65b39a42d58a515eec7ed7ef3deb254ab3
mehdi_807 <mehdij807@gmail.com>
2024-06-24 17:20:56 +0330

    adding ViewModels folder

---
+++ Src/API/Library.API/ViewModels/AdminVm.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace Library.API.ViewModels;
+
+public class AdminVm
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+}
---
+++ Src/API/Library.API/ViewModels/BookVm.cs
@@ -0,0 +1,7 @@
+﻿namespace Library.API.ViewModels;
+
+public class BookVm
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+}
