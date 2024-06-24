3635027b01532efcfd3657035e88865a91e2327b
mehdi_807 <mehdij807@gmail.com>
2024-06-24 17:08:03 +0330

    adding UserDto

--- Src/API/Library.API/Dtos/UserDto.cs
+++ Src/API/Library.API/Dtos/UserDto.cs
@@ -2,9 +2,20 @@
 
 public class UserDto
 {
+    public int Id { get; set; }
     public string Name { get; set; }
     public string LastName { get; set; }
     public string NationalCode { get; set; }
     public string Address { get; set; }
     public int AdminId { get; set; }
 }
+public class UpdateUserDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string NationalCode { get; set; }
+    public string Address { get; set; }
+    public int AdminId { get; set; }
+}
+
